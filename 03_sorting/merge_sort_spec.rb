26f3846ea82281357b4b4f2549cfc3bee17de803
Dave Lively <davelively14@gmail.com>
2017-07-29 12:37:04 -0400

    Updated tests

--- 03_sorting/merge_sort_spec.rb
+++ 03_sorting/merge_sort_spec.rb
@@ -3,7 +3,15 @@ include RSpec
 require_relative 'merge_sort'
 
 describe "#merge_sort" do
-  it "does something" do
+  it "sorts a collection correctly" do
     expect(merge_sort([2, 10, 3, 4, 6, 8, 5, 7, 9, 1])).to eq [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  end
+
+  it "handles an empty collection" do
+    expect(merge_sort([])).to eq []
+  end
+
+  it "handles a collection ordered backwards" do
+    expect(merge_sort([5,4,3,2,1])).to eq [1,2,3,4,5]
   end
 end
