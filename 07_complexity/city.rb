e33ead2dcfc33d0620b401ba722bacdcb7312632
Dave Lively <davelively14@gmail.com>
2018-01-18 22:37:49 -0500

    Added docs

--- 07_complexity/city.rb
+++ 07_complexity/city.rb
@@ -8,12 +8,19 @@ class City
     @visited = false
   end
 
+  # We'll compare nearest neighbor when adding the neighbor to the city.
   def add_neighbor(city, distance)
+    # Set indicies
     low = 0
     high = @neighbors.length - 1
     mid = 0
+
+    # If first neighbor (i.e. @neighbors length - 1 is less than zero), adds
+    # to the @neighbors instance variable.
     @neighbors.push({city: city, distance: distance}) if high < 0
 
+    # Uses binary search to find right location before inserting. Sorting on
+    # insert prevents searching for nearest neighbor later.
     while low <= high
       mid = (high + low) / 2
 
